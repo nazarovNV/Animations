db66514ec8150810e7739bedc9c2d8e96e7b7cf7
nikolay <nazarov.nikolay15@wb.ru>
2024-07-19 16:48:06 +0300

    Делаю отдельно первый шарик и отдельно второй

--- app/src/main/java/ru/otus/animations/components/TiktokView.kt
+++ app/src/main/java/ru/otus/animations/components/TiktokView.kt
@@ -20,7 +20,6 @@ class TiktokView@JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
     private var stroke = DEFAULT_STROKE_WIDTH
     private lateinit var violet_circle_color: Paint
-    private lateinit var pink_circle_color: Paint
 
     init {
         initCircles(attrs, defStyleAttr)
@@ -28,7 +27,6 @@ class TiktokView@JvmOverloads constructor(
 
     private fun initCircles(attrs: AttributeSet?, defStyleAttr: Int) {
         context.withStyledAttributes(attrs, R.styleable.Tiktok_loading_animation, defStyleAttr, R.style.tiktok_loading_animation) {
-            // stroke = getDimensionPixelSize(R.styleable.Rating_rating_stroke_width, DEFAULT_STROKE_WIDTH)
             violet_circle_color = Paint(Paint.ANTI_ALIAS_FLAG).apply {
                 style = Paint.Style.FILL
                 color = getColorOrThrow(R.styleable.Tiktok_loading_animation_violet_circle_color)
@@ -38,17 +36,17 @@ class TiktokView@JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas) {
             canvas.drawCircle(
-                firstCircleX,
+                (width/2).toFloat(),
                 (height / 2).toFloat(),
                 firstCircleRadius,
                 violet_circle_color
             )
-            canvas.translate(dpToPx(SIZE * 2 + DISTANCE).toFloat(), 0f)
+            canvas.translate(dpToPx(SIZE * 2).toFloat(), 0f)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val desiredWidth = 2 * (dpToPx(SIZE) * 2 + dpToPx(DISTANCE)) - dpToPx(DISTANCE)
-        val desiredHeight = dpToPx(SIZE) * 2
+        val desiredWidth = dpToPx(SIZE)
+        val desiredHeight = dpToPx(SIZE)
         setMeasuredDimension(
             resolveSize(desiredWidth, widthMeasureSpec),
             resolveSize(desiredHeight, heightMeasureSpec)
@@ -59,15 +57,13 @@ class TiktokView@JvmOverloads constructor(
     }
 
     companion object {
-        private const val DEFAULT_COUNT = 5
-        private const val DEFAULT_VALUE = 0
         private const val DEFAULT_STROKE_WIDTH = 1
         private const val DISTANCE = 20
-        private const val SIZE = 50
+        private const val SIZE = 150
         private const val TAG = "Rating"
-        private var firstCircleX = 100f
+        private var firstCircleX = SIZE * 1f
+        private var firstCircleRadius = 150f
         private var secondCircleX = 200f
-        private var firstCircleRadius = 50f
         private var secondCircleRadius = 50f
     }
 
